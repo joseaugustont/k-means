fac74792f9c8c1fe784a6ab9167d4e2cbfb624d4
José Augusto <jose.augusto.neto@ccc.ufcg.edu.br>
2019-11-07 23:19:14 -0300

    Novas atualizações

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # k-means
 Agrupamento de imagens com o modelo k-means.
+
+*Inercia* - **4219947.360232187.**
+
+*Silhueta* - **0.24986600875854492.**
+
+*Não zeis por grupo:* **[20, 10, 0, 0]**
+
+*Zeis por grupo:* **[23, 20, 10, 1]**
--- k-means.py
+++ k-means.py
@@ -1,9 +1,12 @@
 import numpy as np
 from numpy import array
-from scipy.cluster.vq import vq, kmeans, whiten, kmeans2
+from scipy.cluster.vq import vq, whiten, kmeans2
 import scipy.stats as sc
 import matplotlib.pyplot as plt
 from skimage import feature
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from sklearn import metrics
 import pandas as pd
 import shutil
 import cv2
@@ -13,7 +16,7 @@ import os
 
 def LBP(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    features = feature.local_binary_pattern(gray, 10, 5, method="default")
+    features = feature.local_binary_pattern(gray, 8, 1, method="uniform")
     return features
 
 types = ('*.tif', '*jpg2')
@@ -28,11 +31,11 @@ structure = {'image_name':[], 'histo-blue':[],'histo-green':[], 'histo-red':[], 
 df = pd.DataFrame(data=structure)
 
 for i in range(len(images)):
-  b = cv2.calcHist([images[i]],[0],None,[256],[0,256])
-  g = cv2.calcHist([images[i]],[1],None,[256],[0,256])
-  r = cv2.calcHist([images[i]],[2],None,[256],[0,256])
+  b = cv2.calcHist([images[i]],[0],None,[64],[0,256])
+  g = cv2.calcHist([images[i]],[1],None,[64],[0,256])
+  r = cv2.calcHist([images[i]],[2],None,[64],[0,256])
   image = LBP(images[i])
-  lbp,bins = np.histogram(image.ravel(),256,[0,256])
+  lbp,bins = np.histogram(image.ravel(),10)
   structure = {'image_name':imagePaths[i], 'histo-blue':b.ravel(),'histo-green':g.ravel(), 'histo-red':r.ravel(), 'histo-lbp':lbp}
   df = df.append(structure, ignore_index=True) 
 
@@ -43,25 +46,54 @@ for i in range(len(whitened_prepare)):
   for j in range(len(whitened_prepare[i])):
     whitened_prepare[i][j] = np.float32(whitened_prepare[i][j])
 
-whitened = [[] for i in range(54)]
+whitened = [[] for i in range(0,len(imagePaths))]
 
 for i in range(len(whitened)):
   a = np.concatenate((whitened_prepare[i][0], whitened_prepare[i][1]), axis=0)
   b = np.concatenate((whitened_prepare[i][2], whitened_prepare[i][3]), axis=0)
   whitened[i] = np.concatenate((a,b), axis=0)
 
-whitened = whiten(whitened)
-clusters= 5
-centroide,label = kmeans2(data=whitened,k=clusters,iter=3000)
+#whitened = whiten(whitened)
+whitened = np.array(whitened)
+clusters = 4
+#centroide,label = kmeans2(data=whitened,k=clusters, iter=30000)
+inertias=[]
+K = range(1,11)
+for i in K:
+    kmeans = KMeans(n_clusters=i, max_iter=1000).fit(whitened)
+    inertias.append(kmeans.inertia_)
+plt.plot(K, inertias, 'bx-')
+plt.xlabel('k')
+plt.ylabel('Sum_of_squared_distances')
+plt.title('Elbow Method For Optimal k')
+plt.savefig('cotovelo.jpg')
+plt.show()
+
+clusters = int(input())
+
+kmeans = KMeans(n_clusters=clusters, max_iter=1000).fit(whitened)
+label = kmeans.labels_
+print("Inercia - {0}. Silhueta - {1}".format(kmeans.inertia_, silhouette_score(whitened, label, metric='euclidean')))
 
 saida = pd.DataFrame(data=[], columns=['image_name', 'label'])
 
-saida['label'] = label
 saida['image_name'] = imagePaths
+saida['label'] = label
 
 saida1 = []
 for i in range(0,clusters):
   saida1.append(saida[saida['label']==i].values)
+
+r = [0 for i in saida1]
+s = [0 for i in saida1]
+for i in range(len(saida1)):
+  for j in range(len(saida1[i])):
+    if "Nzei" in saida1[i][j][0]:
+      r[i] +=1
+    if not("Nzei" in saida1[i][j][0]):
+      s[i] +=1
+
+print (r, s)
 
 classe = 'classe-'
 diretory = 'C:/Users/augus/Documents/ProjPIBIC/k-means/results/'
