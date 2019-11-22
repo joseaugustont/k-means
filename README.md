b4bfc71f9692e52f115314636ae58b84f06bf6df
José Augusto <jose.augusto.neto@ccc.ufcg.edu.br>
2019-11-21 21:17:57 -0300

    Novos resultados, exclusão de conteúdo desnecessário

--- README.md
+++ README.md
@@ -18,3 +18,31 @@ Agrupamento de imagens com o modelo k-means.
 * *Silhueta* - **0.19886364042758942**
 * *Não zeis por grupo* **[8, 0, 18, 4]**
 * *Zeis por grupo* **[15, 10, 20, 8]**
+
+
+### Novos resultados
+
+* Inercia - **44809819.61871628**
+* Silhueta - **0.537004828453064**
+* Acurácia - **0.36459909228441756**
+* f1 score: *ponderada* - **0.2546534680035289** *micro* - **0.36459909228441756**
+* Recall score: *ponderada* - **0.3645990922844175** *micro* - **0.36459909228441756**
+* Precision score: *ponderada* - **0.19565368666029223** *micro* - **0.36459909228441756**
+* Não zeis por grupo: **[346, 0]**
+* Zeis por grupo: **[241, 74]**
+
+![grafico](https://github.com/joseaugustont/k-means/blob/master/cotovelo-new.jpg)
+
+
+### Resultado do agrupamento realizado na classe-0 do agrupamento anterior
+
+* Inercia - **22363256.422574226**
+* Silhueta - **0.2711460292339325**
+* Acurácia - **0.6575809199318569**
+* f1 score: *ponderada* - **0.6598085092045698** *micro* - **0.6575809199318569**
+* Recall score: *ponderada* - **0.6575809199318569** *micro* - **0.6575809199318569**
+* Precision score: *ponderada* - **0.665220347470897** *micro* - **0.6575809199318569**
+* Não zeis por grupo: **[115, 231]**
+* Zeis por grupo: **[155, 86]**
+
+![grafico](https://github.com/joseaugustont/k-means/blob/master/cotovelo-new2.jpg)
--- k-means.py
+++ k-means.py
@@ -5,7 +5,7 @@ import scipy.stats as sc
 import matplotlib.pyplot as plt
 from skimage import feature
 from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score
+from sklearn.metrics import silhouette_score, precision_score, recall_score, f1_score, accuracy_score
 from sklearn import metrics
 import pandas as pd
 import shutil
@@ -20,12 +20,23 @@ def LBP(image):
     return features
 
 types = ('*.tif', '*jpg2')
-diretory = 'C:/Users/augus/Documents/ProjPIBIC/k-means/images/'
+diretory = 'C:/Users/augus/Documents/ProjPIBIC/k-means/zeis/'
 imagePaths = []
 for files in types :
 	imagePaths.extend(sorted(glob.glob(diretory + files)))
 
 images = [cv2.imread('{0}'.format(i),-1) for i in imagePaths]
+y_true = [0 for i in imagePaths]
+
+diretory = 'C:/Users/augus/Documents/ProjPIBIC/k-means/nao-zeis/'
+cont = len(imagePaths)
+imagePaths2 = []
+for files in types:
+  imagePaths2.extend(sorted(glob.glob(diretory + files)))
+for i in imagePaths2:
+  imagePaths.append(i)
+  y_true.append(1)
+  images.append(cv2.imread('{0}'.format(i),-1))
 
 structure = {'image_name':[], 'histo-blue':[],'histo-green':[], 'histo-red':[], 'histo-lbp':[]}
 df = pd.DataFrame(data=structure)
@@ -55,7 +66,7 @@ for i in range(len(whitened)):
 
 #whitened = whiten(whitened)
 whitened = np.array(whitened)
-clusters = 4
+#clusters = 4
 #centroide,label = kmeans2(data=whitened,k=clusters, iter=30000)
 inertias=[]
 K = range(1,11)
@@ -66,14 +77,35 @@ plt.plot(K, inertias, 'bx-')
 plt.xlabel('k')
 plt.ylabel('Sum_of_squared_distances')
 plt.title('Elbow Method For Optimal k')
-plt.savefig('cotovelo.jpg')
+plt.savefig('cotovelo-new2.jpg')
 plt.show()
+
+def optimal_number_of_clusters(wcss):
+    x1, y1 = 2, wcss[0]
+    x2, y2 = 20, wcss[len(wcss)-1]
+
+    distances = []
+    for i in range(len(wcss)):
+        x0 = i+2
+        y0 = wcss[i]
+        numerator = abs((y2-y1)*x0 - (x2-x1)*y0 + x2*y1 - y2*x1)
+        denominator = math.sqrt((y2 - y1)**2 + (x2 - x1)**2)
+        distances.append(numerator/denominator)
+    
+    return distances.index(max(distances)) + 2
+
+print(optimal_number_of_clusters(inertias))
 
 clusters = int(input())
 
 kmeans = KMeans(n_clusters=clusters, max_iter=1000).fit(whitened)
 label = kmeans.labels_
+
 print("Inercia - {0}. Silhueta - {1}".format(kmeans.inertia_, silhouette_score(whitened, label, metric='euclidean')))
+print("Acurácia - {0}".format(accuracy_score(y_true, label)))
+print("f1 score: ponderada - {0}, micro - {1}".format(f1_score(y_true, label, average='weighted'), f1_score(y_true, label, average='micro')))
+print("Recall score: ponderada - {0}, micro - {1}".format(recall_score(y_true, label, average='weighted'), recall_score(y_true, label, average='micro')))
+print("Precision score: ponderada - {0}, micro - {1}".format(precision_score(y_true, label, average='weighted'), precision_score(y_true, label, average='micro')))
 
 saida = pd.DataFrame(data=[], columns=['image_name', 'label'])
 
@@ -88,9 +120,9 @@ r = [0 for i in saida1]
 s = [0 for i in saida1]
 for i in range(len(saida1)):
   for j in range(len(saida1[i])):
-    if "Nzei" in saida1[i][j][0]:
+    if "n-zeis" in saida1[i][j][0]:
       r[i] +=1
-    if not("Nzei" in saida1[i][j][0]):
+    if not("n-zeis" in saida1[i][j][0]):
       s[i] +=1
 
 print (r, s)
